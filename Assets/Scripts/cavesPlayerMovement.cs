0afff6a81b0ba57111779f44099f4366512cc188
James Grenville <144268860+usernamethe1st@users.noreply.github.com>
2023-09-28 11:55:36 -0700

    I had fun

---
+++ Assets/Scripts/cavesPlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public float walkingspeed;
+public float xdir;
+public float xvector;
+//public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+   //    walkingspeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //  Xdir = Input.GetAxis("Horizontal");
+       // xvector = xdir * walkingspeed*Time.deltaTime;
+       // transform.positon = transform.positon + newvector3(xvector, 0, 0);
+    }
+}
