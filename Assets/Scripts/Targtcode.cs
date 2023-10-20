c2698dde1aa439b2647c735ad38bf1201e159612
James Grenville <144268860+usernamethe1st@users.noreply.github.com>
2023-10-20 11:42:19 -0700

    lear rigidbody

--- Assets/Scripts/Targtcode.cs
+++ Assets/Scripts/Targtcode.cs
@@ -1,52 +1,60 @@
-//using System;
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class Targtcode : MonoBehaviour
-//{
- //   public GameObject Fireball;
-  //  public bool iframe;
-  //  public float iframes;
+public class Targtcode : MonoBehaviour
+{
+    public float thrust = 1.1f;
+    public float must = 1.1f;
+    public float luck = 1.1f;
+    public GameObject Fireball;
+    public bool iframe;
+    public float iframes;
+
+    public Rigidbody2D rd;
     // Start is called before the first frame update
-   // void Start()
-   // {
-    //    iframes = 1000;
-     //   iframe = false;
+    void Start()
+    {
+        iframes = 1000;
+        iframe = false;
 
 
-   // }
+    }
 
-    // Update is called once per frame
-    //void Update()
-   // {        if (iframe == true)
-       // {
-        //    iframes = iframes - Time.deltaTime;
-       // }
-    //    if (iframes < 0)
-       // {
-      //      iframes = 1000;
-       //     iframe = false;
-      //  }
-   //     if (iframe == true)
-        //{
-       //     Instantiate(Fireball);
-            //Fireball.GetComponent<Rigidbody2D>().AddForce();
+     //Update is called once per frame
+    void Update()
+    {        if (iframe == true)
+        {
+            iframes = iframes - Time.deltaTime;
+        }
+        if (iframes < 0)
+        {
+            iframes = 1000;
+            iframe = false;
+        }
+        if (iframe == true)
+        {
+            Instantiate(Fireball);
+           // Fireball.GetComponent<Rigidbody2D>();
+           // rd.AddForce(luck, must, thrust, ForceMode2D.Impulse);
 
-        //}
-       // if (iframe == false)
-        //{
+        }
+        if (iframe == false)
+        {
             
-      //    Destroy(Fireball);
-       // }
-   // }
+          Destroy(Fireball);
+        }
+   }
 
-   // private void OnTriggerEnter2D(Collider2D other)
-    //{
-      //  if (other.gameObject.CompareTag("Player"))
-      //  {
-      //      iframe = true;
-//
-      //  }
-    //}
-//}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+           iframe = true;
+
+           
+           
+        }
+    }
+}
